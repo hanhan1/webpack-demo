67be48ebac1dce28dde8b175912f321559454f2d
hanhanhuang <549551740@qq.com>
2017-08-18 17:50:38 +0800

    使用webpack.config.js 和 npm script

--- app/index.js
+++ app/index.js
@@ -1,12 +1,10 @@
 import _ from 'lodash';
-
+import j from 'jquery';
 function component () {
-  var element = document.createElement('div');
-
+  var element = j('<div></div>');
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+  element.html(_.join(["hello","webpack"],''))
+  return element.get(0);
 }
 
 document.body.appendChild(component());
