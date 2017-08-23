206ebc657d97e27326957ff7f58a39f665027970
hanhanhuang <549551740@qq.com>
2017-08-23 15:37:23 +0800

    引入leancloud

--- src/App.js
+++ src/App.js
@@ -1,41 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import 'normalize.css'
 import './reset.css'
+
+
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo:'test',
-      todoList:[
-        {id:1,title:'第一个待办'},
-        {id:2,title:'第二个待办'}
-      ]
+      newTodo:'',
+      todoList:[]
     }
   }
   render() {
-    let todos = this.state.todoList.map((item,index)=>{
+    let todos = this.state.todoList
+    .filter((item)=>!item.deleted)
+    .map((item,index)=>{
       return (
-        <li>
-          <TodoItem todo={item}/>
+        <li key={index}>
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+          onDelete={this.delete.bind(this)}
+          />
         </li>
       )
     })
+    console.log("加了个TODO了－－"+todos)
+
     return (
       <div className="App">
       <h1>我的待办</h1>
       <div className="inputWrapper">
-      <TodoInput content={this.state.newTodo}/> 
+      <TodoInput content={this.state.newTodo} 
+      onChange={this.changeTitle.bind(this)}
+      onSubmit={this.addTodo.bind(this)}/> 
       </div> 
-      <ol>
+      <ol className="todoList">
         {todos}
       </ol>
       </div>
     )
   }
+  componentDidUpdate(){
+  }
+  toggle(e,todo){
+    todo.status = todo.status === 'completed' ? '':'completed'
+    this.setState(this.state)
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo:event.target.value,
+      todoList:this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id:idMaker(),
+    title:event.target.title,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo:'',
+      todoList:this.state.todoList
+    })
+  }
+  delete(event,todo){
+    todo.deleted = true
+    this.setState(this.state)
+  }
 }
 
 export default App;
+
+let id = 0;
+function idMaker(){
+  id += 1
+  return id
+}
---
+++ src/leanCloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage'
+var APP_ID = 'V6pPTg64lUc5ylqgv329lR4E-gzGzoHsz'
+var APP_KEY = '8OPVfG5xnXz5D9QXSjUSEfnQ'
+AV.init({
+  appId:APP_ID,
+  appKey:APP_KEY
+})
+export default AV
