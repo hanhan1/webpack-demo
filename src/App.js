03d0f426778863a6a4d58c1f4138d40d0454431b
hanhanhuang <549551740@qq.com>
2017-08-24 17:16:51 +0800

    完成登录功能，npm run build

--- src/App.js
+++ src/App.js
@@ -5,92 +5,88 @@ import TodoItem from './TodoItem'
 import 'normalize.css'
 import './reset.css'
 import UserDialog from './UserDialog'
-import {getCurrentUser,signOut} from './leanCloud'
+import { getCurrentUser, signOut } from './leanCloud'
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      user:getCurrentUser()||{},
-      newTodo:'',
-      todoList:[]
+      user: getCurrentUser() || {},
+      newTodo: '',
+      todoList: []
     }
   }
   render() {
     let todos = this.state.todoList
-    .filter((item)=>!item.deleted)
-    .map((item,index)=>{
-      return(
-        <li key={index}>
-          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
-          onDelete={this.delete.bind(this)}
-          />
-        </li>
-      )
-  })
-  
-  
+      .filter((item) => !item.deleted)
+      .map((item, index) => {
+        return (
+          <li key={index}>
+            <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+              onDelete={this.delete.bind(this)}
+            />
+          </li>
+        )
+      })
+
+
 
     return (
       <div className="App">
-      <h1>{this.state.user.username||'我'}的待办
-       {this.state.user.id ? <button　onClick={this.signOut.bind(this)}>登出</button>:null}
-      </h1>
-      <div className="inputWrapper">
-      <TodoInput content={this.state.newTodo} 
-      onChange={this.changeTitle.bind(this)}
-      onSubmit={this.addTodo.bind(this)}/> 
-      </div> 
-      <ol className="todoList">
-        {todos}
-      </ol>
-      {this.state.user.id ?null:<UserDialog 
-      onSignUp={this.onSignUp.bind(this)}
-      onSignIn = {this.onSignIn.bind(this)}
-      />}
+        <h1>{this.state.user.username || '我'}的待办
+       {this.state.user.id ? <button 　onClick={this.signOut.bind(this)}>登出</button> : null}
+        </h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
+        </div>
+        <ol className="todoList">
+          {todos}
+        </ol>
+        {this.state.user.id ? null : <UserDialog
+          onSignUp={this.onSignUpOrSignIn.bind(this)}
+          onSignIn={this.onSignUpOrSignIn.bind(this)}
+        />}
       </div>
     )
   }
-  signOut(){
+  signOut() {
     signOut()
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = {}
     this.setState(stateCopy)
   }
-  onSignUp(user){
+  
+  onSignUpOrSignIn(user) {
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = user
     this.setState(stateCopy)
   }
-  onSignIn(user){
-    let stateCopy =JSON.parse(JSON.stringify(this.state))
-    stateCopy.user = user
-    this.setState(stateCopy)
+  componentDidUpdate() {
   }
-  componentDidUpdate(){
-  }
-  toggle(e,todo){
-    todo.status = todo.status === 'completed' ? '':'completed'
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed'
     this.setState(this.state)
   }
-  changeTitle(event){
+  changeTitle(event) {
     this.setState({
-      newTodo:event.target.value,
-      todoList:this.state.todoList
+      newTodo: event.target.value,
+      todoList: this.state.todoList
     })
   }
-  addTodo(event){
+  addTodo(event) {
     this.state.todoList.push({
-      id:idMaker(),
-    title:event.target.title,
+      id: idMaker(),
+      title: event.target.title,
       status: null,
       deleted: false
     })
     this.setState({
-      newTodo:'',
-      todoList:this.state.todoList
+      newTodo: '',
+      todoList: this.state.todoList
     })
   }
-  delete(event,todo){
+  delete(event, todo) {
     todo.deleted = true
     this.setState(this.state)
   }
@@ -99,7 +95,7 @@ class App extends Component {
 export default App;
 
 let id = 0;
-function idMaker(){
+function idMaker() {
   id += 1
   return id
 }
