5cd30edc5396d0e5ee92860a75193f2156d486a5
hanhanhuang <549551740@qq.com>
2017-08-24 16:58:19 +0800

    完成登录功能

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import './UserDialog.css'
-import{signUp}from './leanCloud'
+import { signUp,signIn } from './leanCloud'
 export default class UserDialog extends Component {
   constructor(props) {
     super(props)
     this.state = {
       selected: 'signUp',
-      formData:{
-        username:'',
-        password:'',
+      formData: {
+        username: '',
+        password: '',
       }
     }
   }
@@ -17,69 +17,80 @@ export default class UserDialog extends Component {
       selected: e.target.value
     })
   }
-  signUp(e){
+  signUp(e) {
+    e.preventDefault()
+    let { username, password } = this.state.formData
+    let success = (user) => {
+      console.log(user)
+      this.props.onSignUp.call(null, user)
+    }
+    let error = (error) => {
+      alert(error)
+    }
+    signUp(username, password, success, error)
+  }
+  signIn(e) {
     e.preventDefault()
     let {username,password} = this.state.formData
     let success = (user)=>{
-      console.log(user)
+      this.props.onSignIn.call(null,user)
     }
     let error = (error)=>{
-      console.log(error)
+      alert(error)
     }
-    signUp(username,password,success,error)
+    signIn(username,password,success,error)
   }
-  signIn(e){}
-  changeFormData(key,e){
+  changeFormData(key, e) {
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.formData[key] = e.target.value
     this.setState(stateCopy)
   }
   render() {
     let signUpForm = (
-    <form className="signUp" onSubmit={this.signUp.bind(this)}>
-      <div className="row">
-        <label>用户名</label>
-        <input type="text" value={this.state.formData.username}
-         onChange={this.changeFormData.bind(this,'username')}
-        />
-      </div>
-      <div className="row">
-        <label>密码</label>
-        <input type="password" value={this.state.formData.password}
-        onChange={this.changeFormData.bind(this,'password')} />
-      </div>
-      <div className="row actions">
-        <button type="submit">注册</button>
-      </div>
-    </form>
-    )
-    let signInForm =(
-      <form className="signIn" onSubmit={this.signIn.bind(this)}>
-        <div 　className="row">
+      <form className="signUp" onSubmit={this.signUp.bind(this)}>
+        <div className="row">
           <label>用户名</label>
           <input type="text" value={this.state.formData.username}
-          onChange={this.changeFormData.bind(this,'username')}/>
+            onChange={this.changeFormData.bind(this, 'username')}
+          />
         </div>
-        <div 　className="row">
+        <div className="row">
           <label>密码</label>
           <input type="password" value={this.state.formData.password}
-          onChange={this.changeFormData.bind(this,'password')}/>
+            onChange={this.changeFormData.bind(this, 'password')} />
         </div>
         <div className="row actions">
-          <button 　type="submit">登录</button>
+          <button type="submit">注册</button>
         </div>
       </form>
     )
-   return(
+    let signInForm = (
+      <form className="signIn" onSubmit={this.signIn.bind(this)}>
+        <div className="row">
+          <label>用户名</label>
+          <input type="text" value={this.state.formData.username}
+            onChange={this.changeFormData.bind(this, 'username')} />
+        </div>
+        <div className="row">
+          <label>密码</label>
+          <input type="password" value={this.state.formData.password}
+            onChange={this.changeFormData.bind(this, 'password')} />
+        </div>
+        <div className="row actions">
+          <button type="submit">登录</button>
+        </div>
+      </form>
+    )
+    return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
-          <nav onChange={this.switch.bind(this)}>
-            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'} />注册</label>
-            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'} />登录</label>
+          <nav >
+            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'} onChange={this.switch.bind(this)}/>注册</label>
+            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'} onChange={this.switch.bind(this)}/>登录</label>
           </nav>
           <div className="panes">
-          {this.state.selected === 'signUp' ?signUpForm :null}
-          {this.state.selected === 'signIn' ?signInForm :null}
+            {this.state.selected === 'signUp' ? signUpForm : null}
+            {this.state.selected === 'signIn' ? signInForm : null}
           </div>
         </div>
       </div>
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -20,10 +20,32 @@ export function signUp(username,password,successFn,errorFn){
   return undefined
 }
 
+export function signIn(username,password,successFn,errorFn){
+  AV.User.logIn(username,password).then(function(loginedUser){
+    let user = getUserFromAVUser(loginedUser)
+    successFn.call(null,user)
+  },function(error){
+    errorFn.call(null,error)
+  })
+}
+
+export function signOut(){
+  AV.User.logOut()
+  return undefined
+}
+export function getCurrentUser(){
+  let user = AV.User.current()
+  if(user){
+    return getUserFromAVUser(user)
+  }else{
+    return null
+  }
+}
+
+
 function getUserFromAVUser(AVUser){
   return{
     id:AVUser.id,
     ...AVUser.attributes
   }
-  
 }
